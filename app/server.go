f5e91c9fb4090c77cd4a37fe14954a98792f5cdb
Love Khatri <lovekhatri9211@gmail.com>
2025-01-15 20:29:05 +0530

    Parse API Version#NC5

--- app/server.go
+++ app/server.go
@@ -5,18 +5,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"slices"
 )
 
 type Response struct {
 	message_size   int32
 	correlation_id int32
+	error_code     int16
 }
 
+var supportedApiVersions = []int16{0, 1, 2, 3, 4}
+
 func (r *Response) serialize() []byte {
-	bytes := make([]byte, 8) // Correct length for the byte slice
+	bytes := make([]byte, 10)
 
 	binary.BigEndian.PutUint32(bytes[0:4], uint32(r.message_size))
 	binary.BigEndian.PutUint32(bytes[4:8], uint32(r.correlation_id))
+	binary.BigEndian.PutUint16(bytes[8:10], uint16(r.error_code))
 
 	return bytes
 }
@@ -52,14 +57,22 @@ func handleRequest(conn net.Conn) {
 		fmt.Println("Request too short")
 	}
 
-	var header RequestHeaderv2
+	var header Request
 
+	header.request_api_version = int16(binary.BigEndian.Uint16(request[6:8]))
 	header.correlation_id = int32(binary.BigEndian.Uint32(request[8:12]))
+
+	var errorCode int16 = 0
+	if !slices.Contains(supportedApiVersions, header.request_api_version) {
+		errorCode = 35 // UNSUPPORTED_VERSION
+	}
 
 	response := Response{
 		message_size:   0,
 		correlation_id: header.correlation_id,
+		error_code:     errorCode,
 	}
+	// Validate the API version
 
 	responseBytes := response.serialize()
 
@@ -71,7 +84,7 @@ func handleRequest(conn net.Conn) {
 	conn.Close()
 }
 
-type RequestHeaderv2 struct {
+type Request struct {
 	message_size        int32
 	request_api_key     int16
 	request_api_version int16
