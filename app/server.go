85f1ef283730b96dcd4387dcf7aad12a021c60a3
Love Khatri <lovekhatri9211@gmail.com>
2025-01-15 20:16:27 +0530

    Parse Correlation ID#WA6

--- app/server.go
+++ app/server.go
@@ -42,17 +42,39 @@ func main() {
 }
 
 func handleRequest(conn net.Conn) {
+	request := make([]byte, 1024)
+	n, err := conn.Read(request)
+
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+	}
+	if n < 12 {
+		fmt.Println("Request too short")
+	}
+
+	var header RequestHeaderv2
+
+	header.correlation_id = int32(binary.BigEndian.Uint32(request[8:12]))
+
 	response := Response{
 		message_size:   0,
-		correlation_id: 7,
+		correlation_id: header.correlation_id,
 	}
 
 	responseBytes := response.serialize()
 
-	_, err := conn.Write(responseBytes)
+	_, err = conn.Write(responseBytes)
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 	}
 
 	conn.Close()
+}
+
+type RequestHeaderv2 struct {
+	message_size        int32
+	request_api_key     int16
+	request_api_version int16
+	correlation_id      int32
+	client_id           string
 }
