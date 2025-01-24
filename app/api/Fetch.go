35a2e572154a32b60472e7e47a776c58d9820931
Love Khatri <lovekhatri9211@gmail.com>
2025-01-24 19:50:50 +0530

    Fetch single message from disk#EG2

--- app/api/Fetch.go
+++ app/api/Fetch.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/binary"
 	"fmt"
+	"os"
 
 	"github.com/codecrafters-io/kafka-starter-go/app/types"
 	"github.com/google/uuid"
@@ -14,72 +15,72 @@ func ParseFetchV16Request(body []byte) (FetchV16Request, error) {
 	offset := 0
 
 	req.MaxWaitMs = int32(binary.BigEndian.Uint32(body[offset:]))
-	fmt.Println("MaxWaitMs: ", req.MaxWaitMs, " Offset: ", offset, " Body: ", body[offset:offset+4])
+	//  fmt.Println("MaxWaitMs: ", req.MaxWaitMs, " Offset: ", offset, " Body: ", body[offset:offset+4])
 	offset += 4
 
 	req.MinBytes = int32(binary.BigEndian.Uint32(body[offset:]))
-	fmt.Println("MinBytes: ", req.MinBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
+	//  fmt.Println("MinBytes: ", req.MinBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
 	offset += 4
 
 	req.MaxBytes = int32(binary.BigEndian.Uint32(body[offset:]))
-	fmt.Println("MaxBytes: ", req.MaxBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
+	//  fmt.Println("MaxBytes: ", req.MaxBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
 	offset += 4
 
 	req.IsolationLevel = int8(body[offset])
-	fmt.Println("IsolationLevel: ", req.IsolationLevel, " Offset: ", offset, " Body: ", body[offset:offset+1])
+	//  fmt.Println("IsolationLevel: ", req.IsolationLevel, " Offset: ", offset, " Body: ", body[offset:offset+1])
 	offset++
 
 	req.SessionId = int32(binary.BigEndian.Uint32(body[offset:]))
-	fmt.Println("SessionId: ", req.SessionId, " Offset: ", offset, " Body: ", body[offset:offset+4])
+	//  fmt.Println("SessionId: ", req.SessionId, " Offset: ", offset, " Body: ", body[offset:offset+4])
 	offset += 4
 
 	req.SessionEpoch = int32(binary.BigEndian.Uint32(body[offset:]))
-	fmt.Println("SessionEpoch: ", req.SessionEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
+	//  fmt.Println("SessionEpoch: ", req.SessionEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
 	offset += 4
 
 	var topics []FetchRequestTopic
 	topicsCount, numOfBytes, _ := DecodeSignedVarint(body[offset:])
-	fmt.Println("TopicsCount: ", topicsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
+	//  fmt.Println("TopicsCount: ", topicsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
 	offset += numOfBytes
 
 	for i := 0; i < int(topicsCount); i++ {
 		var topic FetchRequestTopic
 
 		topic.TopicId = uuid.UUID(body[offset : offset+16])
-		fmt.Println("TopicId: ", topic.TopicId, " Offset: ", offset, " Body: ", body[offset:offset+16])
+		//  fmt.Println("TopicId: ", topic.TopicId, " Offset: ", offset, " Body: ", body[offset:offset+16])
 		offset += 16
 
 		var partitions []FetchRequestTopicPartition
 
 		partitionsCount, numOfBytes, _ := DecodeSignedVarint(body[offset:])
-		fmt.Println("PartitionsCount: ", partitionsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
+		//  fmt.Println("PartitionsCount: ", partitionsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
 		offset += numOfBytes
 
 		for j := 0; j < int(partitionsCount); j++ {
 			var partition FetchRequestTopicPartition
 
 			partition.Partition = int32(binary.BigEndian.Uint32(body[offset:]))
-			fmt.Println("Partition: ", partition.Partition, " Offset: ", offset, " Body: ", body[offset:offset+4])
+			//  fmt.Println("Partition: ", partition.Partition, " Offset: ", offset, " Body: ", body[offset:offset+4])
 			offset += 4
 
 			partition.CurrentLeaderEpoch = int32(binary.BigEndian.Uint32(body[offset:]))
-			fmt.Println("CurrentLeaderEpoch: ", partition.CurrentLeaderEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
+			//  fmt.Println("CurrentLeaderEpoch: ", partition.CurrentLeaderEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
 			offset += 4
 
 			partition.FetchOffset = int64(binary.BigEndian.Uint64(body[offset:]))
-			fmt.Println("FetchOffset: ", partition.FetchOffset, " Offset: ", offset, " Body: ", body[offset:offset+8])
+			//  fmt.Println("FetchOffset: ", partition.FetchOffset, " Offset: ", offset, " Body: ", body[offset:offset+8])
 			offset += 8
 
 			partition.LastFetchedEpoch = int32(binary.BigEndian.Uint32(body[offset:]))
-			fmt.Println("LastFetchedEpoch: ", partition.LastFetchedEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
+			//  fmt.Println("LastFetchedEpoch: ", partition.LastFetchedEpoch, " Offset: ", offset, " Body: ", body[offset:offset+4])
 			offset += 4
 
 			partition.LogStartOffset = int64(binary.BigEndian.Uint64(body[offset:]))
-			fmt.Println("LogStartOffset: ", partition.LogStartOffset, " Offset: ", offset, " Body: ", body[offset:offset+8])
+			//  fmt.Println("LogStartOffset: ", partition.LogStartOffset, " Offset: ", offset, " Body: ", body[offset:offset+8])
 			offset += 8
 
 			partition.PartitionMaxBytes = int32(binary.BigEndian.Uint32(body[offset:]))
-			fmt.Println("PartitionMaxBytes: ", partition.PartitionMaxBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
+			//  fmt.Println("PartitionMaxBytes: ", partition.PartitionMaxBytes, " Offset: ", offset, " Body: ", body[offset:offset+4])
 			offset += 4
 
 			partitions = append(partitions, partition)
@@ -87,7 +88,7 @@ func ParseFetchV16Request(body []byte) (FetchV16Request, error) {
 
 		topic.Partitions = partitions
 		topic.TAG_BUFFER = byte(body[offset])
-		fmt.Println("TAG_BUFFER: ", topic.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
+		//  fmt.Println("TAG_BUFFER: ", topic.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
 		offset++
 
 		topics = append(topics, topic)
@@ -98,25 +99,25 @@ func ParseFetchV16Request(body []byte) (FetchV16Request, error) {
 	var forgottenTopicsData []FetchRequestForgottenTopicsData
 
 	forgottenTopicsDataCount, numOfBytes, _ := DecodeSignedVarint(body[offset:])
-	fmt.Println("ForgottenTopicsDataCount: ", forgottenTopicsDataCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
+	//  fmt.Println("ForgottenTopicsDataCount: ", forgottenTopicsDataCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
 	offset += numOfBytes
 
 	for i := 0; i < int(forgottenTopicsDataCount); i++ {
 		var forgottenTopicData FetchRequestForgottenTopicsData
 
 		forgottenTopicData.TopicId = uuid.UUID(body[offset : offset+16])
-		fmt.Println("ForgottenTopicData.TopicId: ", forgottenTopicData.TopicId, " Offset: ", offset, " Body: ", body[offset:offset+16])
+		//  fmt.Println("ForgottenTopicData.TopicId: ", forgottenTopicData.TopicId, " Offset: ", offset, " Body: ", body[offset:offset+16])
 		offset += 16
 
 		var partitions []int32
 
 		partitionsCount, numOfBytes, _ := DecodeSignedVarint(body[offset:])
-		fmt.Println("ForgottenTopicData.PartitionsCount: ", partitionsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
+		//  fmt.Println("ForgottenTopicData.PartitionsCount: ", partitionsCount, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
 		offset += numOfBytes
 
 		for j := 0; j < int(partitionsCount); j++ {
 			partition := int32(binary.BigEndian.Uint32(body[offset:]))
-			fmt.Println("Partition: ", partition, " Offset: ", offset, " Body: ", body[offset:offset+4])
+			//  fmt.Println("Partition: ", partition, " Offset: ", offset, " Body: ", body[offset:offset+4])
 			offset += 4
 
 			partitions = append(partitions, partition)
@@ -124,7 +125,7 @@ func ParseFetchV16Request(body []byte) (FetchV16Request, error) {
 
 		forgottenTopicData.Partitions = partitions
 		forgottenTopicData.TAG_BUFFER = byte(body[offset])
-		fmt.Println("ForgottenTopicData.TAG_BUFFER: ", forgottenTopicData.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
+		//  fmt.Println("ForgottenTopicData.TAG_BUFFER: ", forgottenTopicData.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
 		offset++
 
 		forgottenTopicsData = append(forgottenTopicsData, forgottenTopicData)
@@ -133,17 +134,17 @@ func ParseFetchV16Request(body []byte) (FetchV16Request, error) {
 	req.ForgottenTopicsData = forgottenTopicsData
 
 	rackIdLength, numOfBytes, _ := DecodeSignedVarint(body[offset:])
-	fmt.Println("RackIdLength: ", rackIdLength, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
+	//  fmt.Println("RackIdLength: ", rackIdLength, " Offset: ", offset, " Body: ", body[offset:offset+numOfBytes])
 	offset += numOfBytes
 
 	if rackIdLength > 0 {
 		req.RackId = string(body[offset : offset+int(rackIdLength-1)])
-		fmt.Println("RackId: ", req.RackId, " Offset: ", offset, " Body: ", body[offset:])
+		//  fmt.Println("RackId: ", req.RackId, " Offset: ", offset, " Body: ", body[offset:])
 		offset += int(rackIdLength - 1)
 	}
 
 	req.TAG_BUFFER = byte(body[offset])
-	fmt.Println("TAG_BUFFER: ", req.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
+	//  fmt.Println("TAG_BUFFER: ", req.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset:offset+1])
 	offset++
 
 	return req, nil
@@ -184,7 +185,12 @@ func GenerateFetchResponse(request types.RequestMessage) ([]byte, error) {
 			partitionRes.LogStartOffset = 0
 			partitionRes.AbortedTransactions = []FetchResponseAbortedTransactions{}
 			partitionRes.PreferredReadReplica = -1
-			partitionRes.Records = []byte{}
+
+			fetchedRecords, err := ReadTopicMessages(string(found.Name), partitionRes.PartitionIndex)
+			if err != nil {
+				fmt.Println("Error reading topic messages: ", err.Error())
+			}
+			partitionRes.Records = []FetchResponseRecord{*fetchedRecords}
 
 			partitions = append(partitions, partitionRes)
 		}
@@ -197,8 +203,6 @@ func GenerateFetchResponse(request types.RequestMessage) ([]byte, error) {
 
 	res.Responses = responses
 	res.TAG_BUFFER = 0
-
-	fmt.Printf("Fetch Response: %+v\n", res)
 
 	bytes := res.Serialize()
 
@@ -215,74 +219,74 @@ func (req FetchV16Response) Serialize() []byte {
 
 	binary.BigEndian.PutUint32(body[offset:], uint32(req.ThrottleTimeMs))
 	offset += 4
-	fmt.Println("ThrottleTimeMs: ", req.ThrottleTimeMs, " Offset: ", offset, " Body: ", body[offset-4:offset])
+	//    fmt.Println("ThrottleTimeMs: ", req.ThrottleTimeMs, " Offset: ", offset, " Body: ", body[offset-4:offset])
 
 	binary.BigEndian.PutUint16(body[offset:], uint16(req.ErrorCode))
 	offset += 2
-	fmt.Println("ErrorCode: ", req.ErrorCode, " Offset: ", offset, " Body: ", body[offset-2:offset])
+	//    fmt.Println("ErrorCode: ", req.ErrorCode, " Offset: ", offset, " Body: ", body[offset-2:offset])
 
 	binary.BigEndian.PutUint32(body[offset:], uint32(req.SessionId))
 	offset += 4
-	fmt.Println("SessionId: ", req.SessionId, " Offset: ", offset, " Body: ", body[offset-4:offset])
+	//    fmt.Println("SessionId: ", req.SessionId, " Offset: ", offset, " Body: ", body[offset-4:offset])
 
 	responsesLength := EncodeSignedVarint(len(req.Responses))
 	copy(body[offset:], responsesLength)
 	offset += len(responsesLength)
-	fmt.Println("ResponsesLength: ", len(req.Responses), " Offset: ", offset, " Body: ", body[offset-len(responsesLength):offset])
+	//    fmt.Println("ResponsesLength: ", len(req.Responses), " Offset: ", offset, " Body: ", body[offset-len(responsesLength):offset])
 
 	for _, topic := range req.Responses {
 		copy(body[offset:], topic.TopicId[:])
 		offset += 16
-		fmt.Println("	TopicId: ", topic.TopicId, " Offset: ", offset, " Body: ", body[offset-16:offset])
+		//    fmt.Println("	TopicId: ", topic.TopicId, " Offset: ", offset, " Body: ", body[offset-16:offset])
 
 		partitionsLength := EncodeSignedVarint(len(topic.Partitions))
 		copy(body[offset:], partitionsLength)
 		offset += len(partitionsLength)
-		fmt.Println("	PartitionsLength: ", len(topic.Partitions), " Offset: ", offset, " Body: ", body[offset-len(partitionsLength):offset])
+		//    fmt.Println("	PartitionsLength: ", len(topic.Partitions), " Offset: ", offset, " Body: ", body[offset-len(partitionsLength):offset])
 
 		for _, partition := range topic.Partitions {
 			binary.BigEndian.PutUint32(body[offset:], uint32(partition.PartitionIndex))
 			offset += 4
-			fmt.Println("		PartitionIndex: ", partition.PartitionIndex, " Offset: ", offset, " Body: ", body[offset-4:offset])
+			//    fmt.Println("		PartitionIndex: ", partition.PartitionIndex, " Offset: ", offset, " Body: ", body[offset-4:offset])
 
 			binary.BigEndian.PutUint16(body[offset:], uint16(partition.ErrorCode))
 			offset += 2
-			fmt.Println("		ErrorCode: ", partition.ErrorCode, " Offset: ", offset, " Body: ", body[offset-2:offset])
+			//    fmt.Println("		ErrorCode: ", partition.ErrorCode, " Offset: ", offset, " Body: ", body[offset-2:offset])
 
 			binary.BigEndian.PutUint64(body[offset:], uint64(partition.HighWatermark))
 			offset += 8
-			fmt.Println("		HighWatermark: ", partition.HighWatermark, " Offset: ", offset, " Body: ", body[offset-8:offset])
+			//    fmt.Println("		HighWatermark: ", partition.HighWatermark, " Offset: ", offset, " Body: ", body[offset-8:offset])
 
 			binary.BigEndian.PutUint64(body[offset:], uint64(partition.LastStableOffset))
 			offset += 8
-			fmt.Println("		LastStableOffset: ", partition.LastStableOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
+			//    fmt.Println("		LastStableOffset: ", partition.LastStableOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
 
 			binary.BigEndian.PutUint64(body[offset:], uint64(partition.LogStartOffset))
 			offset += 8
-			fmt.Println("		LogStartOffset: ", partition.LogStartOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
+			//    fmt.Println("		LogStartOffset: ", partition.LogStartOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
 
 			abortedTransactionsLength := EncodeSignedVarint(len(partition.AbortedTransactions))
 			copy(body[offset:], abortedTransactionsLength)
 			offset += len(abortedTransactionsLength)
-			fmt.Println("		AbortedTransactionsLength: ", len(partition.AbortedTransactions), " Offset: ", offset, " Body: ", body[offset-len(abortedTransactionsLength):offset])
+			//    fmt.Println("		AbortedTransactionsLength: ", len(partition.AbortedTransactions), " Offset: ", offset, " Body: ", body[offset-len(abortedTransactionsLength):offset])
 
 			for _, transaction := range partition.AbortedTransactions {
 				binary.BigEndian.PutUint64(body[offset:], uint64(transaction.ProducerId))
 				offset += 8
-				fmt.Println("			ProducerId: ", transaction.ProducerId, " Offset: ", offset, " Body: ", body[offset-8:offset])
+				//    fmt.Println("			ProducerId: ", transaction.ProducerId, " Offset: ", offset, " Body: ", body[offset-8:offset])
 
 				binary.BigEndian.PutUint64(body[offset:], uint64(transaction.FirstOffset))
 				offset += 8
-				fmt.Println("			FirstOffset: ", transaction.FirstOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
+				//    fmt.Println("			FirstOffset: ", transaction.FirstOffset, " Offset: ", offset, " Body: ", body[offset-8:offset])
 
 				body[offset] = transaction.TAG_BUFFER
 				offset++
-				fmt.Println("			TAG_BUFFER: ", transaction.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
+				//    fmt.Println("			TAG_BUFFER: ", transaction.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
 			}
 
 			binary.BigEndian.PutUint32(body[offset:], uint32(partition.PreferredReadReplica))
 			offset += 4
-			fmt.Println("		PreferredReadReplica: ", partition.PreferredReadReplica, " Offset: ", offset, " Body: ", body[offset-4:offset])
+			//    fmt.Println("		PreferredReadReplica: ", partition.PreferredReadReplica, " Offset: ", offset, " Body: ", body[offset-4:offset])
 
 			recordsLength := EncodeSignedVarint(len(partition.Records))
 			copy(body[offset:], recordsLength)
@@ -290,26 +294,44 @@ func (req FetchV16Response) Serialize() []byte {
 			fmt.Println("		RecordsLength: ", len(partition.Records), " Offset: ", offset, " Body: ", body[offset-len(recordsLength):offset])
 
 			if len(partition.Records) > 0 {
-				copy(body[offset:], partition.Records)
-				offset += len(partition.Records)
-				fmt.Println("		Records: ", partition.Records, " Offset: ", offset, " Body: ", body[offset-len(partition.Records):offset])
+				for _, record := range partition.Records {
+					copy(body[offset:], record.Content)
+					offset += len(record.Content)
+					fmt.Println("			Record: ", record.Content, " Offset: ", offset, " Body: ", body[offset-len(record.Content):offset])
+				}
 			}
 
 			body[offset] = partition.TAG_BUFFER
 			offset++
-			fmt.Println("		TAG_BUFFER: ", partition.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
+			//    fmt.Println("		TAG_BUFFER: ", partition.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
 		}
 
 		body[offset] = topic.TAG_BUFFER
 		offset++
-		fmt.Println("	TAG_BUFFER: ", topic.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
+		//    fmt.Println("	TAG_BUFFER: ", topic.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
 	}
 
 	body[offset] = req.TAG_BUFFER
 	offset++
-	fmt.Println("TAG_BUFFER: ", req.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
+	//    fmt.Println("TAG_BUFFER: ", req.TAG_BUFFER, " Offset: ", offset, " Body: ", body[offset-1:offset])
 
 	return body[:offset]
+}
+
+func ReadTopicMessages(topicName string, partitionId int32) (*FetchResponseRecord, error) {
+	fileName := fmt.Sprintf("/tmp/kraft-combined-logs/%s-%d/00000000000000000000.log", topicName, partitionId)
+	fmt.Println("Reading log file: ", fileName)
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading log file: ", err.Error())
+	}
+
+	fmt.Println("Read topic messages: ", data)
+
+	return &FetchResponseRecord{
+		Length:  uint32(len(data)),
+		Content: data,
+	}, nil
 }
 
 type FetchV16Request struct {
@@ -369,8 +391,13 @@ type FetchResponseTopicPartitions struct {
 	LogStartOffset       int64
 	AbortedTransactions  []FetchResponseAbortedTransactions
 	PreferredReadReplica int32
-	Records              []byte
+	Records              []FetchResponseRecord
 	TAG_BUFFER           byte
+}
+
+type FetchResponseRecord struct {
+	Length  uint32
+	Content []byte
 }
 
 type FetchResponseAbortedTransactions struct {
